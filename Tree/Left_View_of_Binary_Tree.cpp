d051681860d2870290619005862a206330ebc363
PragyaTewary <pragyatewary123@gmail.com>
2021-06-14 20:51:35 +0530

    Initial Commit

--- Tree/Left_View_of_Binary_Tree.cpp
+++ Tree/Left_View_of_Binary_Tree.cpp
@@ -1,7 +1,20 @@
 #include<bits//stdc++.h>
 using namespace std;
 
+/*
+Given a Binary Tree, print Left view of it. 
+Left view of a Binary Tree is set of nodes visible when tree is visited from Left side.
+The task is to complete the function leftView(), which accepts root of the tree as argument.
+Left view of following tree is 1 2 4 8.
 
+         1
+       /   \
+     2       3
+   /  \     /  \
+  4    5   6    7
+   \
+    8   
+*/
 
 
 struct Node
